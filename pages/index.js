a2ae8f670e2d3cb7f2f5a6a9ab9106be192f06f8
Nicholas <nickmokisasian@gmail.com>
2020-04-23 08:49:54 +1000

    Loading some of the data into the index file. Currently pondering what difference having getStaticProps() makes, as running next build followed by serve on the 'out' directory seems to return the head title just fine. Which is expected but also unexpected, again making me question getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,17 @@ import Layout from '../components/Layout';
 import {attributes, react } from '../content/pages/home.md';
 
 export default function Home() {
+  console.log(attributes);
   return (
-    <Layout pageTitle="Home" slides="true">
+    <Layout pageTitle={attributes['page-meta']['title']} slides="true">
     
     </Layout>
   )
 }
 
 export const getStaticProps = (params) => {
-  
+
+  console.log(attributes);
 
   return {
     props: {}
